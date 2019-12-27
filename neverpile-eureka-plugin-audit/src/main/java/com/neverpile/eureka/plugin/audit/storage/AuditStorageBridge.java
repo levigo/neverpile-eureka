bae29018ee99dbe44f8a1e0c222d3b39155c7b26
Kevin Hertfelder <k.hertfelder@levigo.de>
2019-12-27 16:44:29 +0100

    JavaDoc for AuditStorageBridge

--- neverpile-eureka-plugin-audit/src/main/java/com/neverpile/eureka/plugin/audit/storage/AuditStorageBridge.java
+++ neverpile-eureka-plugin-audit/src/main/java/com/neverpile/eureka/plugin/audit/storage/AuditStorageBridge.java
@@ -6,21 +6,71 @@ import java.util.Optional;
 
 import com.neverpile.eureka.model.ObjectName;
 import com.neverpile.eureka.plugin.audit.service.AuditEvent;
+import com.neverpile.eureka.plugin.audit.verification.VerificationService;
 
+/**
+ * This interface allows the audit plugin to use a variaty of storage mediums depending on the timplementation.
+ * This interface is used for storing the AuditEvents itself and and the verification used by the {@link VerificationService}
+ */
 public interface AuditStorageBridge {
 
+  /**
+   * The AuditLog Event will be persisted here and can be retrieved later.
+   *
+   * @param auditEvent the audit event to store.
+   */
   public void putAuditEvent(AuditEvent auditEvent);
 
+  /**
+   * Retrieved a persisted AuditLog Event by its ID.
+   *
+   * @param auditId the audit event ID to retrieve the Auditevent for.
+   * @return the requested AuditEvent.
+   */
   public Optional<AuditEvent> getAuditEvent(String auditId);
 
+  /**
+   * Get all AuditEvents registered for the given Document ID as a list.
+   *
+   * @param documentId the document ID to list all audit events for.
+   * @return the requested AuditEvent List.
+   */
   public List<AuditEvent> getDocumentAuditLog(String documentId);
 
+  /**
+   * Store a implementation dependent Verification Object with the given ObjectName as a key for later verification.
+   *
+   * @param key A unique objectName as a key for the given audit verification data.
+   * @param verificationElement InputStream of any verification data.
+   * @param length length of the given InputStream.
+   */
   public void putVerificationElement(ObjectName key, InputStream verificationElement, int length);
 
+  /**
+   * Retrieve a stored Verification Object by its ObjectName as a Key.
+   *
+   * @param key A unique objectName as a key for the requestd audit verification data.
+   * @return the requested Verification data as an InputStream.
+   */
   public Optional<InputStream> getVerificationElement(ObjectName key);
 
+  /**
+   * Update the Current Verification Head of the undelying verification structure.
+   * This head value is used by various verification data structures to append new data.
+   *
+   * @return the head Verification data as an InputStream.
+   */
+  public Optional<InputStream> getHeadVerificationElement();
+
+
+  /**
+   * Update the Current Verification Head of the undelying verification structure.
+   * This head value has to be updated depending on the implementation to ensure new data can be appended consisantly.
+   *
+   * @param verificationElement a new Inputstream representing the new verification data head.
+   * @param length length of the given InputStream.
+   */
   public void updateHeadVerificationElement(InputStream verificationElement, int length);
 
-  public Optional<InputStream> getHeadVerificationElement();
 
 }
