463ee14f8c22b70610201e90596e48ac0987eadd
Kevin Hertfelder <k.hertfelder@levigo.de>
2020-01-08 17:39:37 +0100

    keep helper-lock.

--- neverpile-eureka-ignite/src/main/java/com/neverpile/eureka/ignite/reference/IgniteReference.java
+++ neverpile-eureka-ignite/src/main/java/com/neverpile/eureka/ignite/reference/IgniteReference.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteAtomicReference;
+import org.apache.ignite.IgniteLock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.neverpile.eureka.tx.atomic.DistributedAtomicReference;
@@ -24,16 +25,17 @@ public class IgniteReference<E extends Serializable> implements DistributedAtomi
   }
 
   @PostConstruct
-  public void start(){
-    this.ref = ignite.atomicReference(name, null, true);;
+  public void start() {
+    this.ref = ignite.atomicReference(name, null, true);
   }
 
   @Override
-  public boolean compareAndSet(E expect, E update){
+  public boolean compareAndSet(E expect, E update) {
     return ref.compareAndSet(expect, update);
   }
+
   @Override
-  public E get(){
+  public E get() {
     return ref.get();
   }
 
@@ -44,12 +46,13 @@ public class IgniteReference<E extends Serializable> implements DistributedAtomi
 
   @Override
   public E alterAndGet(ReferenceFunction<E> fn) {
-    ignite.reentrantLock(name + "-reference-helper-lock", true, true, true).lock();
+    IgniteLock helperLock = ignite.reentrantLock(name + "-reference-helper-lock", true, true, true);
     try {
+      helperLock.lock();
       ref.set(fn.apply(ref.get()));
       return ref.get();
-    }finally {
-      ignite.reentrantLock(name + "-reference-helper-lock", true, true, true).unlock();
+    } finally {
+      helperLock.unlock();
     }
   }
 }
