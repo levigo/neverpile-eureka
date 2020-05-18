af4686ce17c01cd9231d4b83c443f839597bfa58
Jörg Henne <j.henne@levigo.de>
2020-05-18 10:10:32 +0200

    Make ModelMapperConfiguration play nicely with others (fusion!)

--- neverpile-eureka-core/src/main/java/com/neverpile/eureka/rest/configuration/ModelMapperConfiguration.java
+++ neverpile-eureka-core/src/main/java/com/neverpile/eureka/rest/configuration/ModelMapperConfiguration.java
@@ -2,114 +2,132 @@ package com.neverpile.eureka.rest.configuration;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
 
 import org.modelmapper.AbstractConverter;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
 
 import com.neverpile.eureka.impl.documentservice.DocumentPdo;
 import com.neverpile.eureka.model.ContentElement;
 import com.neverpile.eureka.model.Document;
 import com.neverpile.eureka.rest.api.document.DocumentDto;
 import com.neverpile.eureka.rest.api.document.content.ContentElementDto;
+import com.neverpile.eureka.rest.api.document.content.ContentElementFacet;
+import com.neverpile.eureka.rest.configuration.ModelMapperConfiguration.DocumentModelMapperConfigurer;
 
 
 @Configuration
+@Import(DocumentModelMapperConfigurer.class)
 public class ModelMapperConfiguration {
   public interface ModelMapperConfigurer {
     void configure(ModelMapper mapper);
   }
 
-  @Autowired(required = false)
-  List<ModelMapperConfigurer> configurers;
+  @Component
+  public static class DocumentModelMapperConfigurer implements ModelMapperConfigurer {
+    private final Optional<ContentElementFacet> contentElementFacet;
+    
+    public DocumentModelMapperConfigurer(@Autowired final Optional<ContentElementFacet> contentElementFacet) {
+      this.contentElementFacet = contentElementFacet;
+    }
+    
+    @Override
+    public void configure(final ModelMapper m) {
+      m.addConverter(new AbstractConverter<Document, DocumentDto>() {
+        @Override
+        protected DocumentDto convert(final Document source) {
+          DocumentDto dto = new DocumentDto();
+          docToDto(source, dto, m);
+          return dto;
+        }
+      });
+      m.addConverter(new AbstractConverter<DocumentPdo, DocumentDto>() {
+        @Override
+        protected DocumentDto convert(final DocumentPdo source) {
+          DocumentDto dto = new DocumentDto();
+          docToDto(source, dto, m);
+          return dto;
+        }
+      });
 
-  @Bean
-  @Qualifier("document")
-  ModelMapper documentMapper() {
-    ModelMapper m = new ModelMapper();
+      m.addConverter(new AbstractConverter<DocumentDto, Document>() {
+        @Override
+        protected Document convert(final DocumentDto source) {
+          Document doc = new Document();
+          dtoToDoc(source, doc, m);
+          return doc;
+        }
+      });
+      m.addConverter(new AbstractConverter<DocumentDto, DocumentPdo>() {
+        @Override
+        protected DocumentPdo convert(final DocumentDto source) {
+          DocumentPdo doc = new DocumentPdo();
+          dtoToDoc(source, doc, m);
+          return doc;
+        }
+      });
+    }
 
-    Converter<Document, DocumentDto> docToDtoConv = new AbstractConverter<Document, DocumentDto>() {
-      @Override
-      protected DocumentDto convert(final Document source) {
-        DocumentDto dto = new DocumentDto();
-        docToDto(source, dto, m);
-        return dto;
-      }
-    };
+    private void dtoToDoc(final DocumentDto source, final Document doc, final ModelMapper m) {
+      doc.setDocumentId(source.getDocumentId());
+      doc.setVersionTimestamp(source.getVersionTimestamp());
 
-    Converter<DocumentDto, Document> dtoToDocConv = new AbstractConverter<DocumentDto, Document>() {
-      @SuppressWarnings("unchecked")
-      @Override
-      protected Document convert(final DocumentDto source) {
-        Document doc = new Document();
-        dtoToDoc(source, doc, m);
-        return doc;
-      }
-    };
-    Converter<DocumentPdo, DocumentDto> pdoToDtoConv = new AbstractConverter<DocumentPdo, DocumentDto>() {
-      @Override
-      protected DocumentDto convert(final DocumentPdo source) {
-        DocumentDto dto = new DocumentDto();
-        docToDto(source, dto, m);
-        return dto;
-      }
-    };
-
-    Converter<DocumentDto, DocumentPdo> dtoToPdoConv = new AbstractConverter<DocumentDto, DocumentPdo>() {
-      @SuppressWarnings("unchecked")
-      @Override
-      protected DocumentPdo convert(final DocumentDto source) {
-        DocumentPdo doc = new DocumentPdo();
-        dtoToDoc(source, doc, m);
-        return doc;
-      }
-    };
-
-    m.addConverter(docToDtoConv);
-    m.addConverter(dtoToDocConv);
-    m.addConverter(pdoToDtoConv);
-    m.addConverter(dtoToPdoConv);
-
-    if (null != configurers)
-      configurers.forEach(c -> c.configure(m));
-
-    return m;
-  }
-
-  private void dtoToDoc(DocumentDto source, Document doc, ModelMapper m) {
-        doc.setDocumentId(source.getDocumentId());
-        doc.setVersionTimestamp(source.getVersionTimestamp());
-
-    ArrayList<ContentElement> contentElemnts = new ArrayList<>();
+      contentElementFacet.ifPresent(f -> {
+        ArrayList<ContentElement> contentElemnts = new ArrayList<>();
         if (source.getFacets().get("contentElements") != null) {
-          for (ContentElementDto dto : (List<ContentElementDto>) source.getFacets().get("contentElements")) {
-        contentElemnts.add(m.map(dto, ContentElement.class));
+          for (ContentElementDto dto : source.getFacetData(f).orElseGet(Collections::emptyList)) {
+            contentElemnts.add(m.map(dto, ContentElement.class));
           }
         }
-    doc.setContentElements(contentElemnts);
+        doc.setContentElements(contentElemnts);
+      });
 
-        doc.setDateCreated((Instant) source.getFacets().get("dateCreated"));
-        doc.setDateModified((Instant) source.getFacets().get("dateModified"));
-      }
-
-  private void docToDto(Document source, DocumentDto dto, ModelMapper m) {
-    dto.setDocumentId(source.getDocumentId());
-    dto.setVersionTimestamp(source.getVersionTimestamp());
-
-    ArrayList<ContentElementDto> contentElemntDtos = new ArrayList<>();
-    if (source.getContentElements() != null) {
-      for (ContentElement ce : source.getContentElements()) {
-        contentElemntDtos.add(m.map(ce, ContentElementDto.class));
-      }
+      doc.setDateCreated((Instant) source.getFacets().get("dateCreated"));
+      doc.setDateModified((Instant) source.getFacets().get("dateModified"));
     }
-    dto.setFacet("contentElements", contentElemntDtos);
 
-    dto.setFacet("dateCreated", source.getDateCreated());
-    dto.setFacet("dateModified", source.getDateModified());
+    private void docToDto(final Document source, final DocumentDto dto, final ModelMapper m) {
+      dto.setDocumentId(source.getDocumentId());
+      dto.setVersionTimestamp(source.getVersionTimestamp());
+
+      ArrayList<ContentElementDto> contentElemntDtos = new ArrayList<>();
+      if (source.getContentElements() != null) {
+        for (ContentElement ce : source.getContentElements()) {
+          contentElemntDtos.add(m.map(ce, ContentElementDto.class));
+        }
+      }
+      dto.setFacet("contentElements", contentElemntDtos);
+
+      dto.setFacet("dateCreated", source.getDateCreated());
+      dto.setFacet("dateModified", source.getDateModified());
+    }
+  }
+
+  @Autowired(required = false)
+  List<ModelMapperConfigurer> configurers;
+  
+  @Autowired
+  ModelMapper modelMapper;
+
+  @Bean
+  @ConditionalOnMissingBean(ModelMapper.class)
+  ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @PostConstruct
+  public void configureModelMapper() {
+    if (null != configurers)
+      configurers.forEach(c -> c.configure(modelMapper));
   }
 }
