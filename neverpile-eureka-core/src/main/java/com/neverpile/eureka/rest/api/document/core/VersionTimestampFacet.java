e66191931332a21b48574ff25e7752d711f2120d
Jörg Henne <j.henne@levigo.de>
2020-05-18 10:11:33 +0200

    Make version timstamp visible to outside

---
+++ neverpile-eureka-core/src/main/java/com/neverpile/eureka/rest/api/document/core/VersionTimestampFacet.java
@@ -0,0 +1,34 @@
+package com.neverpile.eureka.rest.api.document.core;
+
+import java.time.Instant;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.neverpile.eureka.model.Document;
+import com.neverpile.eureka.rest.api.document.DocumentDto;
+import com.neverpile.eureka.rest.api.document.DocumentFacet;
+
+@Component
+public class VersionTimestampFacet implements DocumentFacet<String> {
+  @Override
+  public String getName() {
+    return "versionTimestamp";
+  }
+
+  @Override
+  public JavaType getValueType(final TypeFactory f) {
+    return f.constructType(Instant.class);
+  }
+
+  @Override
+  public void onRetrieve(final Document document, final DocumentDto dto) {
+    dto.setVersionTimestamp(document.getVersionTimestamp());
+  }
+  
+  @Override
+  public String toString() {
+    return "DocumentFacet{" + "name=" + getName() + '}';
+  }
+}
