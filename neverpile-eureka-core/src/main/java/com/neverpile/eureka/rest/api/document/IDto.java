379a9814954e74423bc8b7e3dbdabaf092d5db07
Shahm Ebido <s.ebido@levigo.de>
2023-03-15 12:07:11 +0100

    feat(NF-223)!: Major 1.0

--- neverpile-eureka-core/src/main/java/com/neverpile/eureka/rest/api/document/IDto.java
+++ neverpile-eureka-core/src/main/java/com/neverpile/eureka/rest/api/document/IDto.java
@@ -1,7 +1,7 @@
 package com.neverpile.eureka.rest.api.document;
 
 /**
- * Interface for all all DTOs (data transfer object). Used when Transporting data to and from the application API.
+ * Interface for all DTOs (data transfer object). Used when Transporting data to and from the application API.
  * Especially when storing non standard data for {@link DocumentFacet}s.
  */
 public interface IDto {
