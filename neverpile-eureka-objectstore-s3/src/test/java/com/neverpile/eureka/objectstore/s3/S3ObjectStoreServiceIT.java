b28f59675f4e6d50433fb82ea7e246ef7ca0b60c
Kevin Hertfelder <k.hertfelder@levigo.de>
2023-11-07 12:28:46 +0100

    feat(NF-777): Replace S3Mock with Minio using Fabric8 plugin.

--- neverpile-eureka-objectstore-s3/src/test/java/com/neverpile/eureka/objectstore/s3/S3ObjectStoreServiceIT.java
+++ neverpile-eureka-objectstore-s3/src/test/java/com/neverpile/eureka/objectstore/s3/S3ObjectStoreServiceIT.java
@@ -1,14 +1,13 @@
 package com.neverpile.eureka.objectstore.s3;
 
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.adobe.testing.s3mock.junit4.S3MockRule;
 import com.amazonaws.services.s3.AmazonS3;
 import com.neverpile.eureka.api.objectstore.AbstractObjectStoreServiceTest;
 
@@ -16,16 +15,16 @@ import com.neverpile.eureka.api.objectstore.AbstractObjectStoreServiceTest;
 @SpringBootTest()
 @ContextConfiguration(classes = TestConfiguration.class)
 @EnableAutoConfiguration()
-public class S3ObjectStoreServiceTest extends AbstractObjectStoreServiceTest {
+public class S3ObjectStoreServiceIT extends AbstractObjectStoreServiceTest {
 
   private static final String BUCKET_NAME = "unit-tests";
 
-  @ClassRule
-  public static final S3MockRule s3 = S3MockRule.builder().withHttpPort(9876).build();
+  @Autowired
+  S3ConnectionConfiguration connectionConfiguration;
 
   @Before
   public void createBucket() {
-    AmazonS3 s3Client = s3.createS3Client();
+    AmazonS3 s3Client = connectionConfiguration.createClient();
     try {
       s3Client.listObjects(BUCKET_NAME).getObjectSummaries().forEach(
           (object -> s3Client.deleteObject(BUCKET_NAME, object.getKey())));
